504f339c1b54ab4da2bc06a411e6d5eff598a618
Brian Moseley <bmoseley@buffalo.edu>
2026-03-30 10:05:25 -0400

    js error with the toc

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
 // SMOOTH SCROLLING (with negative scroll of 40 for header)
 
 $(function () {
-    $('a[href*=#]:not([href=#])').click(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
