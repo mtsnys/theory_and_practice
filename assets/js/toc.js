e635b07b7348d8c479285df250593d1a17e65f32
Brian Moseley <bmoseley@buffalo.edu>
2026-04-03 11:15:11 -0400

    layout changes

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,11 +1,16 @@
 // ADDS ACTIVE CLASS TO LINKS WHEN SECTION WITH THE SAME SELECTOR AS THE HREF IS REACHED (CLASS .LINK IS NEEDED ON ALL <a> TAGS)
 
+var tocScrolling = false;
+
 $(document).ready(function () {
     $(window).scroll(function () {
+        $('.back-to-top').toggleClass('visible', $(this).scrollTop() > 100);
+
+        if (tocScrolling) return;
 
         var y = $(this).scrollTop();
 
-        $('.link').each(function (event) {
+        $('.link').each(function () {
             var id = ($(this).attr('href') || '').replace(/^#/, '');
             var el = id ? document.getElementById(id) : null;
             if (el && y >= $(el).offset().top - 40) {
@@ -20,12 +25,16 @@ $(document).ready(function () {
 // SMOOTH SCROLLING (with negative scroll of 40 for header)
 
 $(function () {
-    $('a[href*="#"]:not([href="#"])').click(function () {
+    $('a.link[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var id = this.hash.slice(1);
             var el = id ? document.getElementById(id) : null;
             if (!el) el = document.getElementsByName(id)[0] || null;
             if (el) {
+                $('.link').removeClass('active');
+                $(this).addClass('active');
+                tocScrolling = true;
+                setTimeout(function () { tocScrolling = false; }, 950);
                 $('html,body').animate({
                     scrollTop: ($(el).offset().top - 40)
                 }, 850);
